d9382c43b131ce3dcd2eada4abd19648a73fc0be
Abhay Bhardwaj <abhay.bhardwaj11@gmail.com>
2020-11-18 19:28:01 +0530

    Updates

---
+++ precache-manifest.d00f0e3b7b264c6ccb9b2673e0694372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678de4df6b92cfaf1c673b88ae063441",
+    "url": "/youtube-clone-web-app/index.html"
+  },
+  {
+    "revision": "f5c2464f11c2b1563a6f",
+    "url": "/youtube-clone-web-app/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "187dc7c4b3d11fbb41c5",
+    "url": "/youtube-clone-web-app/static/js/2.0b68acc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone-web-app/static/js/2.0b68acc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c2464f11c2b1563a6f",
+    "url": "/youtube-clone-web-app/static/js/main.891831b0.chunk.js"
+  },
+  {
+    "revision": "1659df29d91a7dc79f4f",
+    "url": "/youtube-clone-web-app/static/js/runtime-main.8dfe6322.js"
+  }
+]);
